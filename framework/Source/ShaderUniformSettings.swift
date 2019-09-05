5827aa6f1c8620524c913c671ee79d1a5363630c
Jonathan Ellis <jon@than.biz>
2019-09-05 15:28:39 +0100

    Add support for [GLfloat] uniforms

--- framework/Source/ShaderUniformSettings.swift
+++ framework/Source/ShaderUniformSettings.swift
@@ -53,6 +53,11 @@ public struct ShaderUniformSettings {
         set(newValue) { uniformValues[index] = newValue }
     }
 
+    public subscript(index:String) -> [GLfloat]? {
+        get { return uniformValues[index] as? [GLfloat]}
+        set(newValue) { uniformValues[index] = newValue }
+    }
+
     public func restoreShaderSettings(_ shader:ShaderProgram) {
         for (uniform, value) in uniformValues {
             switch value {
@@ -63,6 +68,7 @@ public struct ShaderUniformSettings {
                 case let value as Size: shader.setValue(value.toGLArray(), forUniform:uniform)
                 case let value as Matrix4x4: shader.setMatrix(value.toRowMajorGLArray(), forUniform:uniform)
                 case let value as Matrix3x3: shader.setMatrix(value.toRowMajorGLArray(), forUniform:uniform)
+                case let value as [GLfloat]: shader.setValue(value, forUniform: uniform)
                 default: fatalError("Somehow tried to restore a shader uniform value of an unsupported type: \(value)")
             }
         }
